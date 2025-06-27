749bbe53cfb5b09c1dfd7ebe46855b5fe394392e
0r3o <101781117+0r3o-r3vEr5e@users.noreply.github.com>
2025-06-27 22:02:52 +0700

    Rename RandomForestModel.py

--- ML/RandomForestClassifier.py
+++ ML/RandomForestClassifier.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
