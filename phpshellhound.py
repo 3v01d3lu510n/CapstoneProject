a23456a890f432502bfb14d3948c82a0b4f794b4
TRAN DUC NGHIA <111063228+GermanyNghia@users.noreply.github.com>
2025-08-01 21:23:36 +0700

    Update phpshellhound.py

--- phpshellhound.py
+++ phpshellhound.py
@@ -10,7 +10,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), 'ML_Predict'))
 
 from Imports.findwr import find_webroot
 from Yara import yara_utils
-from Imports.hash_utils import handle_hash_generation
+from Imports.hash_utils import handle_hash_generation, print_hashes
 from TELE_BOT.bot import run_bot
 from ML_Predict.predict import ml_scan_and_log as scan_predict_main, yara_scan_and_log as auto_scan
 
@@ -44,7 +44,7 @@ def main():
     parser.add_argument("-y", metavar="Yara", help="Folder/file để quét bằng YARA CLI (Không ghi log)")
     # SHA1-based rule generation
     parser.add_argument("-w", metavar="TARGET", help="Folder/file tạo rule hash")
-    parser.add_argument("-o", action="store_true", help="Output YARA rule file (không nhập sẽ tạo file với date)")
+    parser.add_argument("-o", "--output", nargs='?', const='', metavar="OUTPUT", help="Output YARA rule file (không nhập sẽ tạo file với date)")
 
     # Telegram bot
     parser.add_argument('--bot', action='store_true', help="Chạy Telegram bot.")
@@ -82,7 +82,7 @@ def main():
         yara_utils.scan_with_all_rules(args.y, write_log=False, show_output=True)
         
     elif args.w:
-        if args.o:
+        if args.output:
             handle_hash_generation(args.w)
         else:
             print_hashes(args.w)
